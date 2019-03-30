e6c1f359e88bbd5dec81d426603f472934a2a684
LolyaDi <you@example.com>
2019-03-30 22:26:59 +0600

    Пятнадцатая домашняя работа

---
+++ Home_Work_15/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Home_Work_15
+{
+    [Serializable]
+    public class Book
+    {
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public int Cost { get; set; }
+
+        public int PublicYear { get; set; }
+
+        public Book(string name, int cost, string author, int publicYear)
+        {
+            Name = name;
+            Cost = cost;
+            Author = author;
+            PublicYear = publicYear;
+        }
+    }
+}
---
+++ Home_Work_15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Home_Work_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> bookList = new List<Book>() { new Book("Язык программирования C# 5.0 и платформа .NET 4.5", 9999, "Эндрю Троелсен", 2015) };
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            using (FileStream fileStream = new FileStream("Book.dat", FileMode.OpenOrCreate))
+            {
+                binaryFormatter.Serialize(fileStream, bookList);
+            }
+
+
+            using (FileStream fileStream = new FileStream("Book.dat", FileMode.OpenOrCreate))
+            {
+                
+                List<Book> books = binaryFormatter.Deserialize(fileStream) as List<Book>;
+
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"Название книги - {book.Name}\n");
+                    Console.WriteLine($"Автор книги - {book.Author}\n");
+                    Console.WriteLine($"Год выпуска - {book.PublicYear}\n");
+                    Console.WriteLine($"Цена - {book.Cost}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
